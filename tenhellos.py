cbd4528e6bcfdfd50eaac19a42785475b948696a
chuck <you@example.com>
2022-08-22 16:01:03 -0700

    1.3.2 your first loop

---
+++ tenhellos.py
@@ -0,0 +1,11 @@
+from re import S
+import stdio
+
+stdio.writeln('1st Hello')
+stdio.writeln('2nd Hello')
+stdio.writeln('3rd Hello')
+
+i = 4
+while i <= 10:
+  stdio.writeln(str(i) + 'th Hello')
+  i += 1
